fb6e403e0424c57e28dfeef6911d8890a22c01d5
S@TwIt <satwikmohantyskm@gmail.com>
2022-07-02 23:22:18 +0530

    version1

--- src/components/Lorem.jsx
+++ src/components/Lorem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import data from '../database/data'
 import './lorem.css'
 
@@ -7,38 +7,74 @@ import './lorem.css'
 const Lorem = () => {
     const [paragraph,setData]=useState([])
     const [counter,setCounter]=useState(0)
+    const [value,setvalue]=useState(false)
+    
 
     const submitHandle = (e) =>{
         e.preventDefault()
         
         let index = parseInt(counter)
-        setData(data.splice(0,index))
-        console.log(paragraph,counter)
+        setData(data.slice(0,index))
+        setvalue(true)
+        
+        
 
 
     }
+const Text = () =>{
+    
+    if(counter > 0 && value){
+    return(
+        <div>
+            <h1># {counter} Paragraph are generated</h1>
+           
+        </div>
+    )}
+    else{
+        <div>
+        <h1>We are working on it</h1>
+    </div>
+
+    }
+}
   return (
-    <div>
-    <section>
+    <div className='main'>
+    <section className='container'>
         <div className="nav">
             <h1>Lorem Ipsum Generator</h1>
-        </div>
-        <form onSubmit={submitHandle}>
+            <Text />
+           
+            
+        
+        <form onSubmit={submitHandle} className='form'>
             <label htmlFor="Counter">Paragraph</label>
             <input 
             type="number" 
             id="counter"
             name='counter'
             value={counter}
-            onChange={(e)=>{setCounter(e.target.value)}}
+            onChange={(e)=>{setCounter(e.target.value);setvalue(false)}}
             />
-            <button type='submit'>Generate</button>
+            <button className='btn' type='submit'>Generate</button>
         </form>
+        </div>
        
     </section>
+    <div className="text-class">
     {/* <p>{paragraph.map((x)=>x)}</p> */}
+    <div className="paragraph">
+    {paragraph.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+
+    </div>
+    
+    </div>
+    
     {/*Error*/}
-    <p>{paragraph}</p> 
+    {/* {paragraph.map((item,idx)=>{
+        <p id={idx}>{item}</p>
+    })} */}
     </div>
   )
 }
